e382c3d57d1c6f880b5633553d1d09981f58fc4b
Jaime Andres Velez Osorio <javobqcol@yahoo.es>
2021-12-01 11:36:05 -0500

    cambio en odometro para copiar registro

--- models/fleet_vehicle_odometro.py
+++ models/fleet_vehicle_odometro.py
@@ -6,189 +6,234 @@ from odoo.exceptions import UserError, ValidationError, Warning
 from odoo.osv import expression
 
 class FleetVehiculeOdometer(models.Model):
-  _inherit = 'fleet.vehicle.odometer'
-  _name = 'fleet.vehicle.odometer'
-  _order = 'date desc, value desc'
+    _inherit = 'fleet.vehicle.odometer'
+    _name = 'fleet.vehicle.odometer'
+    _order = 'date desc, value desc'
 
-  company_id = fields.Many2one('res.company', 'Compañia', default=lambda self: self.env.company)
-  value = fields.Float(string='Inicial',
-                         help="Odometro Inicial",
-                         digits=(10, 2),
-                         store=True,
-                         readonly=False,
-                         group_operator="min",
-                        copy=False)
-  value_final = fields.Float(string='Final',
-                               digits=(10, 2),
-                               help="Odometro Final",
-                               readonly=False,
-                               store=True,
-                               copy=False,
-                               group_operator="max")
-  total_unidades = fields.Float("Total",
-                                  help="Total odometro",
-                                  digits=(10, 2),
-                                  compute="_total_horas",
-                                  group_operator="sum",
-                                  readonly=False,
-                                  copy=False,
-                                  store=True)
-  total_standby = fields.Float("Total+SB",
-                                 help="Total standby",
-                                 digits=(10, 2),
-                                 compute="_total_horas",
-                                 group_operator="sum",
-                                 readonly=False,
-                                 copy=False,
-                                 store=True)
-  date = fields.Date(string='Fecha',
-                       default=False)
-  work_id = fields.Many2one(
-      'fleet.vehicle.work',
-      'Trabajo',
-      domain="[('state','=','activo'),('detalle_ids.vehicle_id','=',vehicle_id)]")
-  driver_id = fields.Many2one('res.partner', related=None, string="Conductor", required=False)
-  es_standby = fields.Boolean(string="Standby", default=False)
-  tipo_odometro = fields.Char(string='Tipo odometro', default='odometer')
-  descripcion = fields.Text(string='Notas',
-    placeholder='Cualquier información pertinente respecto al trabajo realizado')
-  recibo = fields.Char(string='Recibo', help="Numero del recibo de la empresa")
-  documentos_ids = fields.Many2many(
-    'ir.attachment',
-    'fleet_vehicle_odometer_attachment_rel',
-    'odometer_id',
-    'attachment_id',
-    string='Recibos')
-  odometer_unit = fields.Char(string="unidades horometro")
-  # able_to_modify_odometer = fields.Boolean(compute='set_access_for_odometer', string='Is user able to modify product?')
-  tiene_adjunto = fields.Boolean(compute='_set_adjunto')
-  gal = fields.Float(string="Galones")
-  liq_id = fields.Many2one('fleet.vehicle.work.liq',
-    'liquidacion Trabajo',
-    domain="[('work_id','=',work_id)]")
-  liq_driver_id = fields.Many2one('fleet.vehicle.driver.liq',
-    'liquidacion Conductor',
-    domain="[('driver_id','=',driver_id)]")
-  motivo = fields.Selection([
-      ('propio', 'Propio de la empresa'),
-      ('ajeno', 'Ajeno a la empresa $')
-  ], 'Motivo', default='ajeno', help='Motivo de standby', required=True)
+    company_id = fields.Many2one(
+        'res.company', 
+        'Compañia', 
+        default=lambda self: 
+        self.env.company
+    )
+    value = fields.Float(
+        string='Inicial',
+        help="Odometro Inicial",
+        digits=(10, 2), 
+        store=True, 
+        readonly=False, 
+        group_operator="min",
+    )
+    value_final = fields.Float(
+        string='Final', 
+        digits=(10, 2), 
+        help="Odometro Final", 
+        readonly=False, 
+        store=True, 
+        group_operator="max"
+    ) 
+    total_unidades = fields.Float(
+        "Total", 
+        help="Total odometro", 
+        digits=(10, 2), 
+        compute="_total_horas", 
+        group_operator="sum", 
+        readonly=False, 
+        copy=False, 
+        store=True
+    ) 
+    total_standby = fields.Float(
+        "Total+SB", 
+        help="Total standby", 
+        digits=(10, 2), 
+        compute="_total_horas", 
+        group_operator="sum", 
+        readonly=False, 
+        copy=False, 
+        store=True
+    )
+    date = fields.Date(
+        string='Fecha',
+        default=False
+    )
+    work_id = fields.Many2one(
+        'fleet.vehicle.work',
+        'Trabajo',
+        domain="[('state','=','activo'),('detalle_ids.vehicle_id','=',vehicle_id)]"
+    )
+    driver_id = fields.Many2one(
+        'res.partner', 
+        related=None, 
+        string="Conductor", 
+        required=False
+    )
+    es_standby = fields.Boolean(
+        string="Standby", 
+        default=False
+    )
+    tipo_odometro = fields.Char(
+        string='Tipo odometro', 
+        default='odometer'
+    )
+    descripcion = fields.Text(
+        string='Notas',
+        placeholder='Cualquier información pertinente respecto al trabajo realizado'
+    )
+    recibo = fields.Char(
+        string='Recibo', 
+        copy=False,
+        help="Numero del recibo de la empresa"
+    )
+    documentos_ids = fields.Many2many( 
+        'ir.attachment', 
+        'fleet_vehicle_odometer_attachment_rel', 
+        'odometer_id', 
+        'attachment_id', 
+        string='Recibos'
+    )
+    odometer_unit = fields.Char(
+        string="unidades horometro"
+    )
+    tiene_adjunto = fields.Boolean(
+        compute='_set_adjunto'
+    )
+    gal = fields.Float(
+        string="Galones"
+    )
+    liq_id = fields.Many2one(
+        'fleet.vehicle.work.liq',
+        'liquidacion Trabajo',
+        domain="[('work_id','=',work_id)]"
+    )
+    liq_driver_id = fields.Many2one(
+        'fleet.vehicle.driver.liq',
+        'liquidacion Conductor',
+        domain="[('driver_id','=',driver_id)]"
+    )
+    motivo = fields.Selection(
+            [('propio', 'Propio de la empresa'), ('ajeno', 'Ajeno a la empresa $')],
+            'Motivo', 
+            default='ajeno', 
+            help='Motivo de standby', 
+            required=True
+    )
+    
+    def copy(self, default=None):
+        if default is None:
+            default={}
+        default['value'] = self.value_final
+        default['date'] = fields.Datetime.from_string(self.date) + relativedelta(days=1)
+        return super(FleetVehiculeOdometer, self).copy(default)
 
-  def _set_adjunto(self):
-    for reg in self:
-      reg.tiene_adjunto = False
-      if reg.documentos_ids:
-        reg.tiene_adjunto = True
+    def _set_adjunto(self):
+        for reg in self:
+            reg.tiene_adjunto = False
+            if reg.documentos_ids:
+                reg.tiene_adjunto = True
+
+    @api.onchange('vehicle_id')
+    def _onchange_vehicle(self):
+        for rec in self:
+            if rec.vehicle_id:
+                rec.odometer_unit = rec.vehicle_id.odometer_unit
+                rec.driver_id = rec.vehicle_id.driver_id.id
+
+    @api.onchange('date')
+    def _onchange_date(self):
+        for rec in self:
+            if rec.date:
+                fecha_actual = fields.Date.context_today(rec)
+                if rec.date > fecha_actual:
+                    return {
+                        'warning': {
+                            'title': 'Error:',
+                            'message': 'Fecha es mayor que la fecha actual', },
+                            'value': {'date': fecha_actual},
+                    }
+                registro = rec.env['fleet.vehicle.odometer'].search([
+                    ('vehicle_id', '=', rec.vehicle_id.id),
+                    ('tipo_odometro', '=', rec.tipo_odometro),
+                    ('date', '<=', rec.date)],
+                    order="date desc",
+                    limit=1
+                )
+                rec.value = (registro.value_final or 0)
+
+    @api.depends('value', 'value_final', 'total_unidades')
+    def _total_horas(self):
+        for rec in self:
+            det = rec.env['fleet.vehicle.work.det'].search([
+                ('vehicle_id', '=', rec.vehicle_id.id),
+                ('work_id', '=', rec.work_id.id),
+                ('inactivo', '=', False)], 
+                limit=1
+            )
+            if rec.value_final != 0:
+                rec.total_unidades = (rec.value_final or 0) - (rec.value or 0)
+                rec.total_standby = rec.total_unidades
+                if (rec.motivo == 'ajeno') and (rec.total_unidades < det.unidades_standby):
+                    rec.total_standby = det.unidades_standby
+
+    @api.onchange('es_standby', 'motivo')
+    def _total_standby(self):
+        for rec in self:
+            det = rec.env['fleet.vehicle.work.det'].search([
+                ('vehicle_id', '=', rec.vehicle_id.id),
+                ('work_id', '=', rec.work_id.id),
+                ('inactivo', '=', False)], limit=1
+            )
+            rec.total_standby = rec.total_unidades or 0
+            if rec.es_standby and rec.motivo == 'ajeno':
+                if rec.total_standby <= det.unidades_standby:
+                    rec.total_standby = det.unidades_standby
+
+    @api.onchange('work_id')
+    def _onchange_work_id(self):
+        for reg in self:
+            registro = reg.env['fleet.vehicle.work.det'].search([
+                ('vehicle_id', '=', reg.vehicle_id.id),
+                ('work_id', '=', reg.work_id.id),
+                ('inactivo', '=', False)], 
+                limit=1
+            )
+            reg.es_standby = registro.standby
+
+    @api.onchange('recibo')
+    def _onchange_inv_ref(self):
+        res = {}
+        for reg in self:
+            if reg.recibo:
+                reg.recibo = reg.recibo.upper().lstrip()
+                hay_recibo = self.search([
+                    ('recibo', '=', reg.recibo),
+                    ('work_id', '=', reg.work_id.id),
+                ])
+                if hay_recibo:
+                    warning = {'title': 'Atención:',
+                        'message': 'En el sistema hay un recibo interno con el numero %s' % (reg.recibo)}
+                    res.update({'warning': warning})
+            return res
 
 
+    @api.constrains('date')
+    def _check_date(self):
+        for record in self:
+            if not record.date: 
+                raise ValidationError("Error, Debe dar un valor de fecha")
 
-  @api.onchange('vehicle_id')
-  def _onchange_vehicle(self):
-    for rec in self:
-      if rec.vehicle_id:
-        rec.odometer_unit = rec.vehicle_id.odometer_unit
-        rec.driver_id = rec.vehicle_id.driver_id.id
-
-
-
- # codigo ok adicional colocar force_save="1" en campo
-
-  # def set_access_for_odometer(self):
-  #   for record in self:
-  #     record['able_to_modify_odometer'] = False
-  #     if self.env.user.has_group('fleet.fleet_group_manager'):
-  #       record['able_to_modify_odometer'] = True
-  #   #
-    # for reg in self:
-    #   # reg.able_to_modify_odometer = reg.env['res.users'].has_group('fleet.fleet_group_manager')
-    #   reg.able_to_modify_odometer = True
-
-  @api.onchange('date')
-  def _onchange_date(self):
-    for rec in self:
-      if rec.date:
-        fecha_actual = fields.Date.context_today(rec)
-        if rec.date > fecha_actual:
-          return {
-            'warning': {'title': 'Error:',
-                        'message': 'Fecha es mayor que la fecha actual', },
-            'value': {'date': fecha_actual},
-          }
-        registro = rec.env['fleet.vehicle.odometer'].search([
-          ('vehicle_id', '=', rec.vehicle_id.id),
-          ('tipo_odometro', '=', rec.tipo_odometro),
-          ('date', '<=', rec.date)],
-          order="date desc",
-          limit=1)
-#        order="value_final desc, value desc",
-        rec.value = (registro.value_final or 0)
-
-
-
-  @api.depends('value', 'value_final', 'total_unidades')
-  def _total_horas(self):
-    for rec in self:
-      det = rec.env['fleet.vehicle.work.det'].search([
-          ('vehicle_id', '=', rec.vehicle_id.id),
-          ('work_id', '=', rec.work_id.id),
-          ('inactivo', '=', False)], limit=1)
-      if rec.value_final != 0:
-        rec.total_unidades = (rec.value_final or 0) - (rec.value or 0)
-        rec.total_standby = rec.total_unidades
-    if (rec.motivo == 'ajeno') and (rec.total_unidades < det.unidades_standby):
-          rec.total_standby = det.unidades_standby
-
-  @api.onchange('es_standby', 'motivo')
-  def _total_standby(self):
-     for rec in self:
-      det = rec.env['fleet.vehicle.work.det'].search([
-        ('vehicle_id', '=', rec.vehicle_id.id),
-        ('work_id', '=', rec.work_id.id),
-        ('inactivo', '=', False)], limit=1)
-      rec.total_standby = rec.total_unidades or 0
-      if rec.es_standby and rec.motivo == 'ajeno':
-        if rec.total_standby <= det.unidades_standby:
-            rec.total_standby = det.unidades_standby
-
-  @api.onchange('work_id')
-  def _onchange_work_id(self):
-    for reg in self:
-      registro = reg.env['fleet.vehicle.work.det'].search([
-        ('vehicle_id', '=', reg.vehicle_id.id),
-        ('work_id', '=', reg.work_id.id),
-        ('inactivo', '=', False)], limit=1)
-      reg.es_standby = registro.standby
-
-  @api.onchange('recibo')
-  def _onchange_inv_ref(self):
-    res = {}
-    for reg in self:
-      if reg.recibo:
-        reg.recibo = reg.recibo.upper().lstrip()
-        hay_recibo = self.search([
-          ('recibo', '=', reg.recibo),
-          ('work_id', '=', reg.work_id.id),
-        ])
-        if hay_recibo:
-          warning = {'title': 'Atención:',
-                     'message': 'En el sistema hay un recibo interno con el numero %s' % (reg.recibo)}
-          res.update({'warning': warning})
-      return res
-
-
-  @api.constrains('date')
-  def _check_date(self):
-    for record in self:
-      if not record.date:
-        raise ValidationError("Error, Debe dar un valor de fecha")
-
-  @api.constrains('value', 'value_final')
-  def _check_value_value_final(self):
-    for record in self:
-      if record.tipo_odometro == 'odometer':
-        if not record.value:
-          raise ValidationError("Error, Debe dar un valor odometro inicial")
-        if not record.value_final:
-          raise ValidationError("Error, Debe dar un valor odometro final")
-        if record.value_final < record.value:
-          raise ValidationError("Error, El odometro final no puede ser menor que el odometro inicial")
+    @api.constrains('value', 'value_final')
+    def _check_value_value_final(self):
+        for record in self:
+            if record.tipo_odometro == 'odometer':
+                if not record.value:
+                    raise ValidationError(
+                        "Error, Debe dar un valor odometro inicial"
+                    )
+                if not record.value_final:
+                    raise ValidationError(
+                        "Error, Debe dar un valor odometro final"
+                    )
+                if record.value_final < record.value:
+                    raise ValidationError(
+                        "Error, El odometro final no puede ser menor que el odometro inicial"
+                    )
